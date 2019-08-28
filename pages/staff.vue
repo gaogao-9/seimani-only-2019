1d851527297dc535e618b793929552be5b529489
gaogao-9 <gaotuki@gmail.com>
2019-08-28 19:17:09 +0900

    スタッフ一覧の見栄えを良くした

--- pages/staff.vue
+++ pages/staff.vue
@@ -1,34 +1,43 @@
 <template>
   <Layout>
-    <v-layout justify-center>
+    <v-layout justify-center align-center fill-height :class="$style.cards">
       <v-flex xs12 sm10 md8 lg6>
         <v-card>
-          <v-container class="headline">
-            主催メンバー
-          </v-container>
-          <v-container grid-list-sm fluid>
-            <v-layout row wrap>
-              <v-flex v-for="{ id } in staff.core" :key="id" xs3 d-flex>
-                <v-hover>
-                  <v-card slot-scope="{ hover }" flat nuxt :href="`https://twitter.com/${id}`" target="_blank" :elevation="hover ? 5 : 0">
-                    <v-img
-                      :src="`/assets/img/staff/${id}.jpg`"
-                      aspect-ratio="1"
-                      class="grey lighten-2">
-                      <v-layout
-                        slot="placeholder"
-                        fill-height
-                        align-center
-                        justify-center
-                        ma-0>
-                        <v-progress-circular indeterminate color="grey lighten-5" />
-                      </v-layout>
-                    </v-img>
-                  </v-card>
-                </v-hover>
-              </v-flex>
-            </v-layout>
-          </v-container>
+          <v-layout justify-center align-center row wrap>
+            <v-flex xs10>
+              <v-card-title>
+                <h2 class="headline" :class="$style.header">
+                  スタッフ一覧
+                </h2>
+              </v-card-title>
+            </v-flex>
+            <v-card-text>
+              <h3 class="title mb-2">
+                主催メンバー
+              </h3>
+              <v-layout row wrap>
+                <v-flex v-for="{ id } in staff.core" :key="id" xs3 d-flex>
+                  <v-hover>
+                    <v-card slot-scope="{ hover }" flat nuxt :href="`https://twitter.com/${id}`" target="_blank" :elevation="hover ? 5 : 0">
+                      <v-img
+                        :src="`/assets/img/staff/${id}.jpg`"
+                        aspect-ratio="1"
+                        class="grey lighten-2">
+                        <v-layout
+                          slot="placeholder"
+                          fill-height
+                          align-center
+                          justify-center
+                          ma-0>
+                          <v-progress-circular indeterminate color="grey lighten-5" />
+                        </v-layout>
+                      </v-img>
+                    </v-card>
+                  </v-hover>
+                </v-flex>
+              </v-layout>
+            </v-card-text>
+          </v-layout>
         </v-card>
       </v-flex>
     </v-layout>
@@ -71,3 +80,15 @@ export default {
   },
 };
 </script>
+
+<style module>
+.cards {
+  & > * {
+  padding: 10px 0;
+  }
+}
+
+.header {
+  margin: 0 auto;
+}
+</style>
