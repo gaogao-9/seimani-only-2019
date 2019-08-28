9dd867cc49e1d82d5bdde50e413c4cb59aa77420
gaogao-9 <gaotuki@gmail.com>
2019-08-28 19:31:05 +0900

    ヒラシェロ位置調整

--- pages/top.vue
+++ pages/top.vue
@@ -21,7 +21,7 @@
         max-height="246px" />
     </div>
     <v-layout justify-space-around align-center wrap>
-      <v-flex xs11 sm8 md6>
+      <v-flex xs11 sm8 md6 lg6>
         <div :class="$style.flyerContainer">
           <v-img
             src="/assets/img/top.jpg"
@@ -38,7 +38,7 @@
           <img src="/assets/img/flyer/white.png" :class="[$style.flyerElement, $style.white]">
         </div>
       </v-flex>
-      <v-flex xs11 sm11 md4 lg3>
+      <v-flex xs11 sm11 md5 lg5>
         <v-layout justify-space-around align-center>
           <a data-size="large" href="https://twitter.com/intent/tweet?text=%E6%94%BF%E5%89%A3%E3%83%9E%E3%83%8B%E3%83%95%E3%82%A7%E3%82%B9%E3%83%86%E3%82%A3%E3%82%A2%E3%82%AA%E3%83%B3%E3%83%AA%E3%83%BC%E5%90%8C%E4%BA%BA%E8%AA%8C%E5%8D%B3%E5%A3%B2%E4%BC%9A%20%E7%B7%8A%E6%80%A5%E4%BA%A4%E6%B5%81%E3%82%A4%E3%83%99%E3%83%B3%E3%83%88%20%E6%BC%95%E3%81%8E%E5%87%BA%E3%81%9B%EF%BC%81%E3%82%BD%E3%82%AF%E3%83%90%E3%82%A4%E6%B5%B7%E2%85%A1%20%E3%80%9C%E5%B8%B0%E3%81%A3%E3%81%A6%E3%81%8D%E3%81%A1%E3%82%83%E3%81%A3%E3%81%A6%E3%80%81%E3%82%A2%E3%82%A4%E3%83%A0%E3%82%BD%E3%82%A6%E3%83%AA%E3%83%BC%EF%BC%81%20%E5%85%AC%E5%BC%8F%E3%82%B5%E3%82%A4%E3%83%88&hashtags=%E6%94%BF%E5%89%A3%E3%83%9E%E3%83%8B%E3%83%95%E3%82%A7%E3%82%B9%E3%83%86%E3%82%A3%E3%82%A2,%E3%82%A2%E3%82%A4%E3%83%A0%E3%82%BD%E3%82%A6%E3%83%AA%E3%83%BC&url=https%3A%2F%2Ffestia.moe%2F&ref_src=twsrc%5Etfw" class="twitter-mention-button" data-show-count="false">Tweet</a>
         </v-layout>
@@ -151,13 +151,13 @@ export default {
 
       @media screen and (min-width: 1264px) {
         & {
-          left: 225px;
+          left: 200px;
         }
       }
 
       @media screen and (min-width: 1904px) {
         & {
-          left: 270px;
+          left: 230px;
         }
       }
     }
@@ -169,19 +169,19 @@ export default {
 
       @media screen and (min-width: 960px) {
         & {
-          left: -495px;
+          left: -530px;
         }
       }
 
       @media screen and (min-width: 1264px) {
         & {
-          left: -630px;
+          left: -685px;
         }
       }
 
       @media screen and (min-width: 1904px) {
         & {
-          left: -950px;
+          left: -1000px;
         }
       }
     }
